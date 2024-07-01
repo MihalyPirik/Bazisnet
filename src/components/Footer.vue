d5ce833d2fd5e9462d7fec51d263b1ea3a358769
Mihaly Pirik <misipirik@gmail.com>
2024-07-01 11:44:52 +0200

    add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        &copy;{{ currentYear }} Pirik Mihály
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    };
+  }
+};
+</script>
+
+<style scoped>
+.footer {
+  background-color: #f8f9fa;
+  padding: 1rem 0;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+  text-align: center;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,11 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+      aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="container-fluid">
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="navbarContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <router-link class="nav-link active" to="/">Nyitólap</router-link>
@@ -19,6 +23,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
